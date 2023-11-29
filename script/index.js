25d1eea8051e45c93fe7fd8e5979c750464aa93c
rayen815 <90983402+rayen815@users.noreply.github.com>
2023-11-29 17:48:52 +0100

    v23.11.29

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 function soon() {
     a=document.getElementsByTagName("a")
     for (var i=0;i<a.length;i++){
-        if( a[i].getAttribute('href')=="soon.html"){
+        if(a[i].getAttribute('href')=="soon.html"){
             a[i].style.backgroundColor="rgba(255, 96, 96, 0.776)"
         }
     }
